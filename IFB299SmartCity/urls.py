d4407e02488a474685e37b0e68944f40a0c5fc57
xanderodempsey <xander-odempsey@hotmail.com>
2017-07-26 13:49:02 +1000

    Update urls.py

--- IFB299SmartCity/urls.py
+++ IFB299SmartCity/urls.py
@@ -1,4 +1,4 @@
-"""IFB299SmartCity URL Configuration
+"""IFB299ToDoBrisbane URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
