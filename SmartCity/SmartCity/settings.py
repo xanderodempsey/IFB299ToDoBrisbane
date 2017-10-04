f4d99010828e63470ae1ef747cc681afb3cfeb5d
xanderodempsey <xander-odempsey@hotmail.com>
2017-10-05 08:46:06 +1000

    Adding db

--- SmartCity/SmartCity/settings.py
+++ SmartCity/SmartCity/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'SmartCity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SmartCity',
+        'USER': 'root',
+        'PASSWORD': 'toor',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
